39d34a4f8844e07d7b38f9f4537c039d961c82e4
Karlo H <karlohorvat3502@gmail.com>
2023-12-07 11:05:15 +0100

    dodavanje handlebars-a

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const Joi = require("joi");
 
-/* GET users listing. */
+const{ db } = require("..//services/db.js");
+
 router.get('/signin', function(req, res, next) {
   res.render('users/signin');
+});
+
+const signinSchema = Joi.object({
+email: Joi.string().email().max(50).required(),
+password: Joi.string().min(3).max(50).required()
+});
+
+router.post("/signin", function(req, res, next) {
+  const result = signinSchema.validate(req.body);
+  if(result.error)  {
+    res.sendStatus(400);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const stmt = db.prepare("SELECT * FROM users WHERE email = ? AND password = ?");
+  const dbresult = stmt.get(email, password);
+  console.log("DB kaže", dbresult);
+
+
+
+res.render("users/signin")
 });
 
 module.exports = router;
