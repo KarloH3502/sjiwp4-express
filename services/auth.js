ba07485a41b46284403fe256e16e0bc0328ba215
Karlo H <karlohorvat3502@gmail.com>
2024-01-25 09:15:30 +0100

    mogućnost promjene imena emaila i lozinke

--- services/auth.js
+++ services/auth.js
@@ -1,6 +1,7 @@
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const jwt = require("jsonwebtoken");
+const {db} = require("./db.js");
 
 function getUserJwt(id, email, name, role, expDays = 7) {
     const tokenData = {
@@ -41,9 +42,21 @@ function authRequired(req, res, next) {
 }
 
 
+function checkEmailUnique(email) {
+    const stmt = db.prepare("SELECT count(*) FROM users WHERE email = ?;");
+    const result = stmt.get(email);
+
+    if (result["count(*)"]>= 1) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 
 module.exports = {
     getUserJwt,
     parseAuthCookie,
-    authRequired
+    authRequired,
+    checkEmailUnique 
 };
